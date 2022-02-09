8987dc6d2be21f1dcc514e19ed67bdf594a3ffd1
mattiapuglisi099@gmail.com <mattiapuglisi099@gmail.com>
2022-02-09 13:58:51 +0100

    Gioco dei dadi

--- js/main.js
+++ js/main.js
@@ -1,18 +1,36 @@
 
-
+alert('Controllo e-mail')
 
 const emailList = ['mattiapuglisi@hotmail.it' , 'pipporossi@libero.com', 'simoneverdi@gmail.it' ]
 const email = prompt('Inserisci email');
 let find = false;
+
 for(let i=0; i<emailList.length; i++){
-    if(email == emailList[i] ){
+    if(email == emailList[i]){
         find = true;
     }
 }
 
-if(find==true){
+if(find == true){
     alert('email trovata');
 }else{
     alert('email non trovata');
 }
 
+
+alert('GIOCO DEI DADI')
+const computerNum =  Math.floor( Math.random()*6 + 1);
+const userNum =  Math.floor( Math.random()*6 + 1);
+
+if(userNum > computerNum){
+    alert(
+        `Il numero dell'utente è: ${userNum}
+         Il numero del computer è: ${computerNum}
+                   Hai vinto`)
+}
+else{
+    alert(
+        `Il numero dell'utente è: ${userNum}
+         Il numero del computer è: ${computerNum}
+                    Hai perso`)
+}
