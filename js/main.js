518b6290946053c7c86f306b35eaa15b740a9a8c
mattiapuglisi099@gmail.com <mattiapuglisi099@gmail.com>
2022-02-09 15:02:49 +0100

    Sistemato in html e css

--- js/main.js
+++ js/main.js
@@ -1,36 +1,57 @@
 
-alert('Controllo e-mail')
-
+//EMAIL
 const emailList = ['mattiapuglisi@hotmail.it' , 'pipporossi@libero.com', 'simoneverdi@gmail.it' ]
-const email = prompt('Inserisci email');
-let find = false;
+const input = document.getElementById('input-email');
+const button = document.getElementById('conferma');
 
-for(let i=0; i<emailList.length; i++){
-    if(email == emailList[i]){
-        find = true;
+button.addEventListener('click', function(){
+    let find = false;
+    const email = input.value;
+    if(!email)
+    {
+        alert('inserisci email');
     }
-}
+    else{
+        for(let i=0; i<emailList.length; i++){
+            if(email == emailList[i]){
+                find = true;
+            }
+        }
+        if(find == true){
+            alert('email trovata');
+        }else{
+            alert('email non trovata');
+        }
 
-if(find == true){
-    alert('email trovata');
-}else{
-    alert('email non trovata');
-}
+    }
+    console.log(email)
+})
 
 
-alert('GIOCO DEI DADI')
-const computerNum =  Math.floor( Math.random()*6 + 1);
-const userNum =  Math.floor( Math.random()*6 + 1);
 
-if(userNum > computerNum){
-    alert(
-        `Il numero dell'utente è: ${userNum}
-         Il numero del computer è: ${computerNum}
-                   Hai vinto`)
-}
-else{
-    alert(
-        `Il numero dell'utente è: ${userNum}
-         Il numero del computer è: ${computerNum}
-                    Hai perso`)
-}
+
+
+
+//DADI
+
+const buttonGioca = document.getElementById('gioca');
+const sectionDadi = document.getElementById('dadi')
+buttonGioca.addEventListener('click', function(){
+    const computerNum =  Math.floor( Math.random()*6 + 1);
+    const userNum =  Math.floor( Math.random()*6 + 1);
+    sectionDadi.innerHTML += "<h3> Il numero del computer è: "+ computerNum +"</h3>";
+    sectionDadi.innerHTML += "<h3> Il tuo numero è: "+ userNum +"</h3>";
+    
+    if(userNum == computerNum){
+        sectionDadi.innerHTML += "<h2>Hai pareggiato</h2>";
+    }
+    else if(userNum > computerNum){
+        sectionDadi.innerHTML += "<h2>Hai vinto</h2>";
+    }else{
+        sectionDadi.innerHTML += "<h2>Hai perso</h2>";
+    }
+
+        
+    
+})
+
